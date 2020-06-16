8e2ef90f4a22455ac41eff889a7ee3e50b5c6006
limyipd70270 <60630395+limyipd70270@users.noreply.github.com>
2020-06-16 12:01:28 +1200

    allows 'xxx' as exit code and numbers for the lengths
    also allows for other letters, need to change later, meant to only accept numbers above 0 and 'xxx'

---
+++ 01_new_user.py
@@ -0,0 +1,22 @@
+def not_blank(question, error_msg, num_ok):
+    error = error_msg
+
+    valid = False
+    while not valid:
+        response = input(question)
+        has_errors = ""
+
+        if num_ok != "yes":
+            # look at each character in string and if it's a number, complain
+            for letter in response:
+                if letter.isdigit() == True:
+                    has_errors = "yes"
+                    break
+
+        if response == "":
+            print(error)
+            continue
+        elif has_errors != "":
+            print(error)
+        else:
+            return response
--- 04_shape_dimensions.py
+++ 04_shape_dimensions.py
@@ -1,43 +1,50 @@
-# Number checking function (number must be a float that is more than 0)
-def num_check(question):
-    error = "Please enter a number that is more than zero"
+def not_blank(question, error_msg, num_ok):
+    error = error_msg
 
     valid = False
     while not valid:
-        try:
-            response = float(input(question))
+        response = input(question)
+        has_errors = ""
 
-            if response <= 0:
-                print(error)
-            else:
-                return response
+        if num_ok != "yes":
+            # look at each character in string and if it's a number, complain
+            for letter in response:
+                if letter.isdigit() == True:
+                    has_errors = "yes"
+                    break
 
-        except ValueError:
+        if response == "":
             print(error)
-
+            continue
+        elif has_errors != "":
+            print(error)
+        else:
+            return response
 
 # Initialise lists
 # all_lengths includes every shapes' dimensions
 all_lengths = []
 
 # get all dimensions, will use the needed dimensions later to calculate the perimeter
-print("Please enter the lengths for your shape (or radius if it's a circle), pressing 'enter' after each one. Print the number '000' once you have entered all of them")
+print("Please enter the lengths (don't need to include the unit of measurement) for your shape (or radius if it's a circle), pressing 'enter' after each one. Print the number 'xxx' once you have entered all of them")
 
 length = ""
-while length != "000":
+while length != "xxx":
     # shapes_lengths is the list for the individual shape's lengths added
     shapes_lengths = []
 
     # length is for each side of the shape
-    length = num_check("Length: ")
+    length = not_blank("Length: ", "Please enter a number that is more than zero", "yes")
 
     # If user enters exit code, break out of loop
-    if length == "000":
+    if length == "xxx":
         break
 
     shapes_lengths.append(length)
     all_lengths.append(shapes_lengths)
 
-print(shapes_lengths)
+# should work if shapes_lengths???
 print(all_lengths)
+
+
 
